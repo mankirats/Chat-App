0d6427c9f2bd7215726e7803f01f00c6cc154496
Mankirat Singh <mankirat95@gmail.com>
2022-10-01 18:19:23 +0530

    UserSideBar component added

---
+++ src/Shared/Components/UserSidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./CustomStyle";
+
+function UserSidebar(props) {
+    return <Container>Send or Start a new Chat</Container>;
+}
+
+export default UserSidebar;
