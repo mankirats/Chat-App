1d1216829ea6ba2048d4e27dbc66a64c785e03ed
Mankirat Singh <mankirat95@gmail.com>
2022-10-01 18:18:53 +0530

    MessageSideBar component added

---
+++ src/Shared/Components/MessageSideBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { MessageSideBarWrapper } from "./CustomStyle";
+
+function MessageSideBar() {
+    return <MessageSideBarWrapper>MessageSideBar</MessageSideBarWrapper>;
+}
+
+export default MessageSideBar;
