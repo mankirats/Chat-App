9940bb64e1ba36d12a06e4b1d768819b3047ddf7
Mankirat Singh <mankirat95@gmail.com>
2022-10-01 23:37:29 +0530

    Header Component Fixed

--- src/Shared/Components/Header.js
+++ src/Shared/Components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HeaderWrapper } from "./CustomStyle";
 
 function Header() {
-    return <HeaderWrapper>Header</HeaderWrapper>;
+    return <HeaderWrapper>Chat App</HeaderWrapper>;
 }
 
 export default Header;
