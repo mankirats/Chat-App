09f71f611457ed0737fb465c0525a01e093f9367
Mankirat Singh <mankirat95@gmail.com>
2022-10-02 10:55:12 +0530

    Typechat component Added

---
+++ src/Shared/Components/TypeChat.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TypeChatInput } from "./CustomStyle";
+function TypeChat(props) {
+    return (
+        <>
+            <TypeChatInput placeholder="Type a message"></TypeChatInput>
+        </>
+    );
+}
+
+export default TypeChat;
