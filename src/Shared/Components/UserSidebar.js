82099ec500d67dff1d55299bedcc394589c214d5
Mankirat Singh <mankirat95@gmail.com>
2022-10-02 10:55:31 +0530

    UserSideBar Component Updated

--- src/Shared/Components/UserSidebar.js
+++ src/Shared/Components/UserSidebar.js
@@ -1,8 +1,60 @@
 import React from "react";
-import { Container } from "./CustomStyle";
-
+import { Container, TitleContainer, SearchContactInput } from "./CustomStyle";
+import MessageSideView from "./MessageSideView";
 function UserSidebar(props) {
-    return <Container>Send or Start a new Chat</Container>;
+    return (
+        <>
+            <TitleContainer fontWeight="600" textAlign="center">
+                <SearchContactInput
+                    type="text"
+                    name="contact"
+                    placeholder="&#128269; Send or Start a new Chat"
+                />
+            </TitleContainer>
+            <Container>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+                <MessageSideView
+                    senderName="Alex"
+                    datetime="10-09-2022"
+                    message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam harum suscipit dolore perferendis quia rerum a vitae assumenda blanditiis voluptatem, velit veniam maiores minima, iusto alias, nulla sed corrupti dolores."
+                ></MessageSideView>
+            </Container>
+        </>
+    );
 }
 
 export default UserSidebar;
